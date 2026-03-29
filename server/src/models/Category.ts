337dd9724b125feed345922aca3c79579fc80641
devlxn <hallstaffxcigil@gmail.com>
2026-03-29 18:04:45 +0300

    Delete server directory

--- server/src/models/Category.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const categorySchema = new Schema({
-  name: { type: String, required: true, unique: true, trim: true },
-  description: { type: String, default: "" },
-  createdAt: { type: Date, default: Date.now },
-});
-
-export default model("Category", categorySchema);
